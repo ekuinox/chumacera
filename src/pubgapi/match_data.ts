c2aa25fe34ef663aaab4eec8992ddc61295f5a80
lm9 <depkey@me.com>
2018-04-06 04:06:34 +0900

    relationshipsの修正

--- src/pubgapi/match_data.ts
+++ src/pubgapi/match_data.ts
@@ -17,7 +17,9 @@ namespace PUBGAPI {
         };
         readonly relationships: {
             assets: any[];
-            matches: any[];
+            rosters: Roster[];
+            rounds: any[];
+            spectators: any[];
         };
         readonly links: {
             shema?: string;
@@ -31,7 +33,31 @@ namespace PUBGAPI {
             if (data.type !== this.type) throw new Error("Data isn't Match's");
             this.id = data.id;
             this.attributes = data.attributes;
-            this.relationships = data.relationships;
+            this.relationships = { assets : [], rosters: [], rounds: [], spectators: [] };
+            // assets
+            if (data.relationships.assets && data.relationships.assets.data) {
+                data.relationships.assets.data.forEach((asset: any) => {
+                    if (asset.type === "asset") this.relationships.assets.push(asset);
+                });
+            }
+            // rosters
+            if (data.relationships.rosters && data.relationships.rosters.data) {
+                data.relationships.rosters.data.forEach((roster: any) => {
+                    this.relationships.rosters.push(new Roster(roster));
+                });
+            }
+            // rounds
+            if (data.relationships.rounds && data.relationships.rounds.data) {
+                data.relationships.rounds.data.forEach((round: any) => {
+                    if (round.type === "round") this.relationships.rounds.push(round);
+                });
+            }
+            // spectators
+            if (data.relationships.spectators && data.relationships.spectators.data) {
+                data.relationships.spectators.data.forEach((spectator: any) => {
+                    if (spectator.type === "spectator") this.relationships.spectators.push(spectator);
+                });
+            }
             this.links = data.links;
             included.forEach((v: any) => {
                 if (v.type === "roster") this.rosters.push(new Roster(v));
--- src/tests/test1.ts
+++ src/tests/test1.ts
@@ -6,16 +6,11 @@ const APIKEY = JSON.parse(fs.readFileSync("./conf/key.json", "utf8"))["APIKEY"];
 const client = new Client(APIKEY);
 
 (async () => {
-    client.getPlayer("aaaccount.0992f709b65e4ad294663022efe57958", "pc-jp")
-    .then(player => {
-        console.log(player);
-        /*
-        player.relationships.matches.forEach(async match => {
-            client.getMatch(match.id, "pc-jp")
-            .then(m => {
-                fs.writeFileSync(`./samples/match_${m.id}.json`, JSON.stringify(m));
-            }).catch(e => console.log(e));
-        });
-        */
-    }).catch(e => console.log(e));
+	client.getPlayers("pc-as", {names: ["lm0x"]})
+	.then(players => {
+		client.getMatch("pc-as", players[0].relationships.matches[0].id)
+		.then(match => {
+			console.log(match.relationships);
+		});
+	}).catch(e => console.log(e));
 })();
