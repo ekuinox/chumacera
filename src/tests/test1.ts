1b516f0d370eb6dc8f5123a6c81cfbb3d49d5db2
lm9 <depkey@me.com>
2018-04-05 15:16:38 +0900

    request -> axios & qs

--- src/tests/test1.ts
+++ src/tests/test1.ts
@@ -6,13 +6,16 @@ const APIKEY = JSON.parse(fs.readFileSync("./conf/key.json", "utf8"))["APIKEY"];
 const client = new Client(APIKEY);
 
 (async () => {
-    client.getPlayer("account.0992f709b65e4ad294663022efe57958", "pc-jp")
+    client.getPlayer("aaaccount.0992f709b65e4ad294663022efe57958", "pc-jp")
     .then(player => {
+        console.log(player);
+        /*
         player.relationships.matches.forEach(async match => {
             client.getMatch(match.id, "pc-jp")
             .then(m => {
                 fs.writeFileSync(`./samples/match_${m.id}.json`, JSON.stringify(m));
             }).catch(e => console.log(e));
         });
+        */
     }).catch(e => console.log(e));
 })();
