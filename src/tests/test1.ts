5df880f0f4e03ce429d6ed20d53ac2fc379f01c5
lm9 <depkey@me.com>
2018-04-06 20:28:59 +0900

    Telemetry URL どうやって取得したらいいのですか．．．

--- src/tests/test1.ts
+++ src/tests/test1.ts
@@ -8,9 +8,10 @@ const client = new Client(APIKEY);
 (async () => {
 	client.getPlayers("pc-as", {names: ["lm0x"]})
 	.then(players => {
+		console.log("pc-as", players[0].relationships.matches[0].id);
 		client.getMatch("pc-as", players[0].relationships.matches[0].id)
 		.then(match => {
-			console.log(match.relationships);
+			console.log(match.relationships.assets);
 		});
 	}).catch(e => console.log(e));
 })();
