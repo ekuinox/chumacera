2f21949d4b66f020717fde5a94e4a12edc642687
lm9 <depkey@me.com>
2018-04-07 02:18:58 +0900

    sample吐き出し

--- src/tests/test3.ts
+++ src/tests/test3.ts
@@ -9,7 +9,7 @@ const client = new Client(APIKEY);
     client.getMatch("pc-jp", "447ef3e9-ee54-4791-9549-b9cf1b951b28")
     .then(res => {
         client.getTelemetry(res.assets[0].attributes.URL)
-        .then(res => console.log(res));
+        .then(res => fs.writeFileSync(`./samples/telemetry_${res.data[0].MatchId}.json`, JSON.stringify(res)));
     })
     .catch(e => console.log(e));
 })();
