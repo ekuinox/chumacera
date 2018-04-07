43b1fc40e787dae4e9e814279bfea60bcf28b472
lm9 <depkey@me.com>
2018-04-07 17:06:44 +0900

    はい

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,38 @@ export { PlayerData } from "./pubgapi/player_data";
 export { Roster } from "./pubgapi/roster";
 export { Asset } from "./pubgapi/asset";
 export { TelemetryData } from "./pubgapi/telemetry_data";
+export {
+	Common,
+	ItemPackage,
+	Item,
+	Character,
+	Vehicle,
+	GameState,
+	Location
+} from "./pubgapi/telemetry_objects";
+export {
+    Basic as EventBasic,
+    LogPlayerLogin,
+    LogPlayerCreate,
+    LogPlayerPosition,
+    LogPlayerAttack,
+    LogItemPickup,
+    LogItemEquip,
+    LogItemUnequip,
+    LogVehicleRide,
+    LogMatchDefinition,
+    LogMatchStart,
+    LogGameStatePeriodic,
+    LogVehicleLeave,
+    LogPlayerTakeDamage,
+    LogPlayerLogout,
+    LogItemAttach,
+    LogItemDrop,
+    LogPlayerKill,
+    LogItemDetach,
+    LogItemUse,
+    LogCarePackageSpawn,
+    LogVehicleDestroy,
+    LogCarePackageLand,
+    LogMatchEnd
+} from "./pubgapi/telemetry_events";
