ae8c21349e836f37193feeca1d8d58c1b4a45e58
lm9 <depkey@me.com>
2018-04-07 17:44:57 +0900

    test対応

--- src/tests/test3.ts
+++ src/tests/test3.ts
@@ -8,8 +8,10 @@ const client = new Client(APIKEY);
 (async () => {
     client.getMatch("pc-jp", "447ef3e9-ee54-4791-9549-b9cf1b951b28")
     .then(res => {
-        client.getTelemetry(res.assets[0].attributes.URL)
+        client.getTelemetry(res.assets[0].attributes.URL);
+        /* 古いです
         .then(res => fs.writeFileSync(`./samples/telemetry_${res.data[0].MatchId}.json`, JSON.stringify(res)));
+        */
     })
     .catch(e => console.log(e));
 })();
--- src/tests/test5.ts
+++ src/tests/test5.ts
@@ -9,9 +9,17 @@ const telemetry_url_filtered = "https://api.pubg.report/bluehole-pubg/pc-as/2018
 const telemetry_url = "https://telemetry-cdn.playbattlegrounds.com/bluehole-pubg/pc-as/2018/04/05/18/50/2fdff74e-3902-11e8-8f50-0a586466e49f-telemetry.json";
 
 (async () => {
+    /*
     client.getTelemetry(telemetry_url)
     .then(res => {
         console.log(res.events);
+    })
+    .catch(e => console.log(e));
+    */
+   client.getMatch("pc-jp", "447ef3e9-ee54-4791-9549-b9cf1b951b28")
+    .then(res => {
+        res.assets[0].getTelemetry()
+        .then(res => console.log(res.events));
     })
     .catch(e => console.log(e));
 })();
