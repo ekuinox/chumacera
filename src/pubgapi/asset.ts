a98ca4c3744bea70d4f1ea6e05e00296ec593b79
lm9 <depkey@me.com>
2018-04-07 21:52:12 +0900

    ここもifで囲っとく

--- src/pubgapi/asset.ts
+++ src/pubgapi/asset.ts
@@ -5,7 +5,7 @@ namespace PUBGAPI {
     export class Asset {
         readonly type: string = "asset";
         readonly id: string;
-        readonly attributes: {
+        readonly attributes?: {
             URL: string;
             createdAt: string;
             description: string;
@@ -15,12 +15,14 @@ namespace PUBGAPI {
         constructor(data: any) {
             if (data.type !== this.type) throw new Error("Data isn't Asset's");
             this.id = data.id;
-            this.attributes = {
-                URL: data.attributes.URL,
-                createdAt: data.attributes.createdAt,
-                description: data.attributes.description,
-                name: data.attributes.name
-            };
+            if (data.attributes) {
+                this.attributes = {
+                    URL: data.attributes.URL,
+                    createdAt: data.attributes.createdAt,
+                    description: data.attributes.description,
+                    name: data.attributes.name
+                };
+            }
         }
 
         public getTelemetry() {
