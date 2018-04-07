c144f3daba3c73d42ff5045f2d9d60bc00fff6fa
lm9 <depkey@me.com>
2018-04-07 21:39:14 +0900

    "true", "false"

--- src/pubgapi/roster.ts
+++ src/pubgapi/roster.ts
@@ -1,23 +1,34 @@
 namespace PUBGAPI {
-    export class Roster {
-        readonly type: string = "roster";
-        readonly id: string;
-        readonly attributes: {
-            shardId: string;
-            stats: any;
-            won: string; // TODO : TO boolean
-        };
-        readonly relationships: {
-            participants: any; // めんどい
-            team: any
-        }
-        constructor(data: any) {
-            if (data.type !== this.type) throw new Error("Data isn't Roster's");
-            this.id = data.id;
-            this.attributes = data.attributes;
-            this.relationships = data.relationships;
-        }
-    }
+	export class Roster {
+		readonly type: string = "roster";
+		readonly id: string;
+		readonly attributes?: {
+			shardId: string;
+			stats: any;
+			won: boolean; // TODO : TO boolean
+		};
+		readonly relationships?: {
+			participants: any; // めんどい
+			team: any
+		}
+		constructor(data: any) {
+			if (data.type !== this.type) throw new Error("Data isn't Roster's");
+			this.id = data.id;
+			if (data.attributes) {
+				this.attributes = {
+					shardId: data.attributes.shardId,
+					stats: data.attributes.stats,
+					won: data.attributes.won === "true" ? true : false
+				};
+			}
+			if (data.relationships) {
+				this.relationships = {
+					participants: data.relationships.participants,
+					team: data.relationships.team
+				};
+			}
+		}
+	}
 }
 
 export = PUBGAPI;
--- src/tests/test3.ts
+++ src/tests/test3.ts
@@ -8,10 +8,7 @@ const client = new Client(APIKEY);
 (async () => {
     client.getMatch("pc-jp", "447ef3e9-ee54-4791-9549-b9cf1b951b28")
     .then(res => {
-        client.getTelemetry(res.assets[0].attributes.URL);
-        /* 古いです
-        .then(res => fs.writeFileSync(`./samples/telemetry_${res.data[0].MatchId}.json`, JSON.stringify(res)));
-        */
+        console.log(res);
     })
     .catch(e => console.log(e));
 })();
