8a0d65d5993640f998bef9aa8cca889e5860cdb1
lm9 <depkey@me.com>
2018-04-09 16:04:27 +0900

    ParticipantStatsつくった

--- src/pubgapi/participant.ts
+++ src/pubgapi/participant.ts
@@ -1,11 +1,69 @@
 namespace PUBGAPI {
+    export class ParticipantStats {
+        readonly DBNOs: number; // ダウン復帰回数っぽい
+        readonly assists: number;
+        readonly boosts: number;
+        readonly damage_dealt: number;
+        readonly death_type: string; // alive, byplayer, suicideの三値
+        readonly headshot_kills: number;
+        readonly heals: number;
+        readonly kill_place: number;
+        readonly kill_points_delta: number;
+        readonly kill_streaks: number;
+        readonly kills: number;
+        readonly last_kill_points: number;
+        readonly longest_kill: number;
+        readonly most_damage: number;
+        readonly name: string; // username
+        readonly player_id: string;
+        readonly revives: number; // DBNOsとの違いがわからん
+        readonly ride_distance: number;
+        readonly road_kills: number;
+        readonly team_kills: number;
+        readonly time_survived: number;
+        readonly vehicle_destroys: number;
+        readonly walk_distance: number;
+        readonly weapons_acquired: number;
+        readonly win_place: number;
+        readonly win_points_delta: number;
+
+        constructor(data: any) {
+            this.DBNOs = data.DBNOs;
+            this.assists = data.assists;
+            this.boosts = data.boosts;
+            this.damage_dealt = data.damageDealt;
+            this.death_type = data.deathType;
+            this.headshot_kills = data.headshotKills;
+            this.heals = data.heals;
+            this.kill_place = data.killPlace;
+            this.kill_points_delta = data.killPointsDelta;
+            this.kill_streaks = data.killStreaks;
+            this.kills = data.kills;
+            this.last_kill_points = data.lastKillPoints;
+            this.longest_kill = data.longestKill;
+            this.most_damage = data.mostDamage;
+            this.name = data.name;
+            this.player_id = data.playerId;
+            this.revives = data.revives;
+            this.ride_distance = data.ride_distance;
+            this.road_kills = data.roadKills;
+            this.team_kills = data.teamKills;
+            this.time_survived = data.teamSurvived;
+            this.vehicle_destroys = data.vihicleDestroys;
+            this.walk_distance = data.walkDistance;
+            this.weapons_acquired = data.weaponsAcquired;
+            this.win_place = data.winPlace;
+            this.win_points_delta = data.winPoints;
+        }
+    }
+
     export class Participant {
         readonly type: string = "participant";
         readonly id: string;
         readonly attributes?: {
-            actor: string;
+            actor: string; // 基本的に空っぽい
             shardId: string;
-            stats: any; 
+            stats: ParticipantStats;
         };
         constructor(data: any) {
             if (data.type !== this.type) throw new Error("Data isn't Participant's");
@@ -14,7 +72,7 @@ namespace PUBGAPI {
                 this.attributes = { 
                     actor: data.attributes.actor,
                     shardId: data.attributes.shardId,
-                    stats: data.attributes.stats
+                    stats: new ParticipantStats(data.attributes.stats)
                 };
             }
             
--- src/tests/test7.ts
+++ src/tests/test7.ts
@@ -1,16 +1,21 @@
-import { Client } from "../app";
+import { Client, Participant } from "../app";
 import fs from "fs-extra";
 
 const APIKEY = JSON.parse(fs.readFileSync("./conf/key.json", "utf8"))["APIKEY"];
 
 const client = new Client(APIKEY);
 
+// マッチ参加者としての情報を抜き出すサンプル
 (async () => {
     client.getPlayers("pc-jp", {names: ["lm0x"]})
     .then(res => {
         res[0].relationships.matches[0].getMatch()
         .then(match => {
-            console.log(match.attributes);
+            match.participants.forEach(participant => {
+                if (participant.attributes && participant.attributes.stats.name === "lm0x") {
+                    console.log(participant.attributes.stats);
+                }
+            });
         })
     })
     .catch(e => console.log(e));
