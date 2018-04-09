9239f5e8097f22c2582675418f400a1316fa5d87
lm9 <depkey@me.com>
2018-04-09 15:37:35 +0900

    はい

--- src/pubgapi/match_data.ts
+++ src/pubgapi/match_data.ts
@@ -1,73 +1,88 @@
 import { Client, Asset, Roster, Participant } from "../app";
 
 namespace PUBGAPI {
-    export class MatchData {
-        private client: Client;
-        readonly type: string = "match";
-        readonly id: string;
-        readonly attributes: {
-            createdAt: string;
-            duration: number;
-            gameMode: string;
-            patchVersion?: string;
-            shardId: string;
-            stats?: any;
-            tags?: any;
-            titleId: string;     
-        };
-        readonly relationships: {
-            assets: any[];
-            rosters: Roster[];
-            rounds: any[];
-            spectators: any[];
-        };
-        readonly links: {
-            shema?: string;
-            self: string;
-        }
+	export class MatchData {
+		private client: Client;
+		readonly type: string = "match";
+		readonly id: string;
+		readonly attributes: {
+			created_at: string;
+			duration: number;
+			game_mode: string; // Enumとあるんだけど，EventModeでまたいろいろ返ってくるからstringにしとくのが無難っぽい
+			patch_version: string;
+			shard_id: string;
+			stats: any;
+			tags: any;
+			title_id: string;
+		};
+		readonly relationships: {
+			assets: {type: string, id: string}[];
+			rosters: Roster[];
+			rounds: any[];
+			spectators: any[];
+		};
+		readonly links: {
+			shema?: string;
+			self: string;
+		}
 
-        readonly participants: Participant[] = [];
-        readonly rosters: Roster[] = [];
-        readonly assets: Asset[] = [];
+		readonly participants: Participant[] = [];
+		readonly rosters: Roster[] = [];
+		readonly assets: Asset[] = [];
 
-        constructor(client: Client, data: any, included: any[]) {
-            this.client = client;
-            if (data.type !== this.type) throw new Error("Data isn't Match's");
-            this.id = data.id;
-            this.attributes = data.attributes;
-            this.relationships = { assets : [], rosters: [], rounds: [], spectators: [] };
-            // assets
-            if (data.relationships.assets && data.relationships.assets.data) {
-                data.relationships.assets.data.forEach((asset: any) => {
-                    if (asset.type === "asset") this.relationships.assets.push(asset);
-                });
-            }
-            // rosters
-            if (data.relationships.rosters && data.relationships.rosters.data) {
-                data.relationships.rosters.data.forEach((roster: any) => {
-                    this.relationships.rosters.push(new Roster(roster));
-                });
-            }
-            // rounds
-            if (data.relationships.rounds && data.relationships.rounds.data) {
-                data.relationships.rounds.data.forEach((round: any) => {
-                    if (round.type === "round") this.relationships.rounds.push(round);
-                });
-            }
-            // spectators
-            if (data.relationships.spectators && data.relationships.spectators.data) {
-                data.relationships.spectators.data.forEach((spectator: any) => {
-                    if (spectator.type === "spectator") this.relationships.spectators.push(spectator);
-                });
-            }
-            this.links = data.links;
-            included.forEach((v: any) => {
-                if (v.type === "roster") this.rosters.push(new Roster(v));
-                else if (v.type === "participant") this.participants.push(new Participant(v));
-                else if (v.type === "asset") this.assets.push(new Asset(this.client, v));
-            })
-        }
-    }
+		constructor(client: Client, data: any, included: any[]) {
+			this.client = client;
+			if (data.type !== this.type) throw new Error("Data isn't Match's");
+			this.id = data.id;
+			this.attributes = {
+				created_at: data.attributes.createdAt,
+				duration: data.attributes.duration,
+				game_mode: data.attributes.gameMode,
+				patch_version: data.attributes.patchVersion,
+				shard_id: data.attributes.shardId,
+				stats: data.attributes.stats,
+				tags: data.attributes.tags,
+				title_id: data.attributes.titleId
+			};
+
+			this.relationships = { assets: [], rosters: [], rounds: [], spectators: [] };
+			// assets
+			if (data.relationships.assets && data.relationships.assets.data) {
+				data.relationships.assets.data.forEach((asset: any) => {
+					if (asset.type === "asset") {
+						this.relationships.assets.push({
+							type: asset.type,
+							id: asset.id
+						});
+					}
+				});
+			}
+			// rosters
+			if (data.relationships.rosters && data.relationships.rosters.data) {
+				data.relationships.rosters.data.forEach((roster: any) => {
+					this.relationships.rosters.push(new Roster(roster));
+				});
+			}
+			// rounds
+			if (data.relationships.rounds && data.relationships.rounds.data) {
+				data.relationships.rounds.data.forEach((round: any) => {
+					if (round.type === "round") this.relationships.rounds.push(round);
+				});
+			}
+			// spectators
+			if (data.relationships.spectators && data.relationships.spectators.data) {
+				data.relationships.spectators.data.forEach((spectator: any) => {
+					if (spectator.type === "spectator") this.relationships.spectators.push(spectator);
+				});
+			}
+			this.links = data.links;
+			included.forEach((v: any) => {
+				if (v.type === "roster") this.rosters.push(new Roster(v));
+				else if (v.type === "participant") this.participants.push(new Participant(v));
+				else if (v.type === "asset") this.assets.push(new Asset(this.client, v));
+			})
+		}
+	}
 }
 
 export = PUBGAPI;
---
+++ src/tests/test7.ts
@@ -0,0 +1,17 @@
+import { Client } from "../app";
+import fs from "fs-extra";
+
+const APIKEY = JSON.parse(fs.readFileSync("./conf/key.json", "utf8"))["APIKEY"];
+
+const client = new Client(APIKEY);
+
+(async () => {
+    client.getPlayers("pc-jp", {names: ["lm0x"]})
+    .then(res => {
+        res[0].relationships.matches[0].getMatch()
+        .then(match => {
+            console.log(match.attributes);
+        })
+    })
+    .catch(e => console.log(e));
+})();
